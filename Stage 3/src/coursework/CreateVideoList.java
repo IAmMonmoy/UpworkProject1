d132b8be1796e91ca5aca9699fb7824554c4148a
Shoukhin <shoukhin1993@gmail.com>
2017-03-29 14:10:05 +0600

    initializing stage 3

---
+++ Stage 3/src/coursework/CreateVideoList.java
@@ -0,0 +1,52 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package coursework;
+
+import java.awt.BorderLayout;
+import java.awt.HeadlessException;
+import java.awt.TextArea;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.*;
+
+/**
+ *
+ * @author Shoukhin
+ */
+public class CreateVideoList extends JFrame implements ActionListener {
+
+    JTextField trackNo = new JTextField(2);
+    TextArea information = new TextArea(6, 50);
+    JButton list = new JButton("Reset playlist");
+    JButton check = new JButton("Add to playlist");
+
+    public CreateVideoList()  {
+        setLayout(new BorderLayout());
+        setBounds(100, 100, 400, 200);
+        setTitle("Create video playlist");
+        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        JPanel top = new JPanel();
+        top.add(new JLabel("Enter Video Number:"));
+        top.add(trackNo);
+        top.add(check);
+        top.add(list);
+        list.addActionListener(this);
+        check.addActionListener(this);
+        add("North", top);
+        JPanel middle = new JPanel();
+        information.setText("");
+        middle.add(information);
+        add("Center", middle);
+
+        setResizable(false);
+        setVisible(true);
+    }
+    
+    
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+    }
+}
