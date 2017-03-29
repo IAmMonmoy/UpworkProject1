71672c15a95b7d0a860fd75fef2bfb93ad0e4453
jaggesher <jaggesher14@gmail.com>
2017-03-29 15:28:19 +0600

    Init VideoUpdate

--- Stage 2/src/coursework/VideoPlayer.java
+++ Stage 2/src/coursework/VideoPlayer.java
@@ -48,6 +48,9 @@ public class VideoPlayer extends JFrame implements ActionListener {
         else if(e.getSource() == playlist){
             new CreateVideoList();
         }
+        else if(e.getSource()==update){
+            new UpdateVideos();
+        }
         
         else if (e.getSource() == quit) {
             VideoData.close();
